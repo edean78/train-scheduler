fc72a23c460843bd6a93715402656679fd63ea6c
edean78 <edean78@gmail.com>
2019-10-07 16:02:24 -0400

    Added firebase config to app.js file, and added a few styles to styles.css

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+// Ready the document
+// $(document).ready(funtion(){
+
+    // Your web app's Firebase configuration
+    var config = {
+        apiKey: "AIzaSyDAyTeBA-TJJcv-xXgELWP8_pQy9pWnEkA",
+        authDomain: "train-scheduler-81a1c.firebaseapp.com",
+        databaseURL: "https://train-scheduler-81a1c.firebaseio.com",
+        projectId: "train-scheduler-81a1c",
+        storageBucket: ""
+    };
+
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    
+
+// });
