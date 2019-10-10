a00dd2da3792ae26bf3a901893de23f435e42e47
edean78 <edean78@gmail.com>
2019-10-09 21:39:52 -0400

    Created Initial values, fixed css link to display styles, and added an id to submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,16 +3,28 @@
 
     // Your web app's Firebase configuration
     var config = {
-        apiKey: "",
+        apiKey: "AIzaSyCynWVZa4c50o3U0LXoKlAGnh2bHcfQMUo",
         authDomain: "train-scheduler-81a1c.firebaseapp.com",
         databaseURL: "https://train-scheduler-81a1c.firebaseio.com",
         projectId: "train-scheduler-81a1c",
         storageBucket: ""
     };
 
+    // Initialize firebase
     firebase.initializeApp(config);
 
+    // Create a variable to reference the database.
     var database = firebase.database();
+
+    // Set initial values
+    var trainName = "";
+    var destination = "";
+    var firstTrainTime = "";
+    var frequency = "";
+
+    // Create button click
+
+    
 
 
 
